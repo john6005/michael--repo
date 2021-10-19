95c09e9a90762b4b966cb628d0df2e933b549f7d
9Flights <john.net6005@gmail.com>
2021-10-19 20:55:08 +0530

    added username and password into main

---
+++ WebApplication1/user.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class user
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
